1e669f58d4def058c6fd8beebf24113914546dd9
Veinz Alucard <65653624+khoidh24@users.noreply.github.com>
2026-01-03 10:37:21 +0700

    Update notify.lua

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,15 +1,30 @@
 return {
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			vim.notify = require("notify")
-			require("notify").setup({
-				stages = "slide",
-				timeout = 3000,
-				background_colour = "#000000",
-			})
-		end,
-	},
+{
+    "rcarriga/nvim-notify",
+    config = function()
+      vim.notify = require("notify")
+      local notify = require("notify")
+
+      require("notify").setup({
+        stages = "slide",
+        timeout = 3000,
+        background_colour = "#000000",
+      })
+
+      vim.api.nvim_create_autocmd("RecordingEnter", {
+        callback = function()
+          local reg = vim.fn.reg_recording()
+          notify("Recording macro @" .. reg, vim.log.levels.INFO, { title = "Macro", timeout = false })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("RecordingLeave", {
+        callback = function()
+          notify("Stopped recording", vim.log.levels.INFO, { title = "Macro", timeout = 1000 })
+        end,
+      })
+    end,
+  },
 	{
 		"stevearc/dressing.nvim",
 	},
